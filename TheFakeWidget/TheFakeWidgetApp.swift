487d35ea8f330aed9eff808c3159bb89e99f4a4d
JinYuchyi <yuqi_jin@apple.com>
2022-07-13 21:11:32 +0800

    Initial Commit

---
+++ TheFakeWidget/TheFakeWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheFakeWidgetApp.swift
+//  TheFakeWidget
+//
+//  Created by Yuqi Jin on 13/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct TheFakeWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
